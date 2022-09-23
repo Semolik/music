8a1a2bf3951863e74bd9cc4ff0b5123c5114f8c7
Semolik <70706334+Semolik@users.noreply.github.com>
2022-09-23 22:36:18 +0700

    Update security.py

--- backend/core/security.py
+++ backend/core/security.py
@@ -1,5 +1,8 @@
 from passlib.context import CryptContext
+from sqlalchemy.orm.session import Session
+from typing import Optional, MutableMapping, List, Union
 
+from backend.schemas.user import UserInfo
 
 PWD_CONTEXT = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -10,3 +13,17 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def get_password_hash(password: str) -> str:
     return PWD_CONTEXT.hash(password)
+
+
+def authenticate(
+    *,
+    email: str,
+    password: str,
+    db: Session,
+) -> Optional[UserInfo]:
+    user = db.query(User).filter(User.email == email).first()
+    if not user:
+        return None
+    if not verify_password(password, user.hashed_password):  # 1
+        return None
+    return user
