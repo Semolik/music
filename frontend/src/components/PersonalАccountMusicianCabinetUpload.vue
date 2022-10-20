d07858179665567654e97a115e3af3c52189b7e5
Semolik <70706334+Semolik@users.noreply.github.com>
2022-10-20 12:48:00 +0700

    upd

--- frontend/src/components/PersonalАccountMusicianCabinetUpload.vue
+++ frontend/src/components/PersonalАccountMusicianCabinetUpload.vue
@@ -1,3 +1,28 @@
 <template>
-    Загрузить треки
+    <div class="buttons">
+        <div :class="['button', {active: activeSelection==='single'}]" @click="activeSelection='single'">
+            <div class="text">Сингл</div>
+        </div>
+        <div :class="['button', {active: activeSelection==='album'}]" @click="activeSelection='album'">
+            <div class="text">Альбом</div>
+        </div>
+    </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            activeSelection: 'single',
+        }
+    }
+}
+</script>
+<style lang="scss">
+.buttons {
+    grid-template-columns: repeat(2, 1fr);
+
+    .button.active {
+        background-color: var(--purple);
+    }
+}
+</style>
