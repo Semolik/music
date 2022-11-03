5612d18329e09078d1351c8bc785f07d75509b32
Semolik <70706334+Semolik@users.noreply.github.com>
2022-11-03 10:41:37 +0700

    upd

--- backend/core/config.py
+++ backend/core/config.py
@@ -3,7 +3,7 @@ from typing import List, Literal, Optional, Tuple, get_args
 from dotenv import dotenv_values
 
 
-env_config = dotenv_values('.env')
+env_config = {**dotenv_values('.env'), **dotenv_values(".env.local"), }
 
 
 class Settings(BaseSettings):
@@ -11,8 +11,8 @@ class Settings(BaseSettings):
     SERVER_LINK: str = 'http://localhost:3000'
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
         'http://localhost:4000', 'http://192.168.50.106:4000', 'http://192.168.1.133:4000']
-    DATABASE_URI: Optional[str] = "postgresql://postgres:aboba@localhost:5432/semolik_music"
-    TEST_DATABASE_URI: Optional[str] = "postgresql://postgres:aboba@localhost:5432/semolik_music_test"
+    DATABASE_URI: Optional[str] = f"postgresql://{env_config['DB_USER']}:{env_config['DB_PASSWORD']}@{env_config['DB_HOST']}:{env_config['DB_PORT']}/{env_config['DB_NAME']}"
+    # TEST_DATABASE_URI: Optional[str] = "postgresql://postgres:aboba@localhost:5432/semolik_music_test"
     FIRST_SUPERUSER: str = "admin"
     ASSETS_FOLDER: str = 'assets/'
     IMAGES_FOLDER: str = ASSETS_FOLDER+'images'
