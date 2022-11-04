80ba7fa5d498df5ad358d1f0fb691201860f4385
Semolik <70706334+Semolik@users.noreply.github.com>
2022-11-04 19:44:02 +0700

    refactoring

--- backend/helpers/forms.py
+++ backend/helpers/forms.py
@@ -4,8 +4,8 @@ from fastapi import Form
 def form_body(cls):
     cls.__signature__ = cls.__signature__.replace(
         parameters=[
-            arg.replace(default=Form(...) if arg.default !=
-                        None else Form(None))
+            arg.replace(default=Form(None) if arg.default is
+                        None else Form(...))
             for arg in cls.__signature__.parameters.values()
         ]
     )
