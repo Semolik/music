bb6880c7b459e7f6700b2334eb0ec5ce6c0e310f
Semolik <70706334+Semolik@users.noreply.github.com>
2023-03-22 17:28:38 +0700

    :recycle: refactoring

--- frontend/composables/authMiddleware.js
+++ frontend/composables/authMiddleware.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { useAuthStore } from "@/stores/auth";
 import { storeToRefs } from "pinia";
 import { routesNames, navigateTo } from "@typed-router";
@@ -7,39 +6,14 @@ export const useAuthMiddleware = async (context, userType) => {
     const { logout, refresh } = authStore;
     const { logined, userData } = storeToRefs(authStore);
     if (process.server) {
-        const cookie = useCookie("access_token_cookie");
-        if (cookie.value) {
-            try {
-                const request = await axios.get(
-                    "http://localhost:8000/api/v1/users/me",
-                    {
-                        withCredentials: true,
-                        headers: {
-                            Cookie: `access_token_cookie=${cookie.value}`,
-                        },
-                    }
-                );
-                if (request.status === 200) {
-                    logined.value = true;
-                }
-
-                if (userType && request.data.type !== userType) {
-                    logout();
-                    return navigateTo({ name: routesNames.login });
-                }
-            } catch (error) {}
-
-            if (!logined.value) {
-                logout();
-                return navigateTo({ name: routesNames.login });
-            }
+        await authStore.getUserData();
+        if ((userType && userData?.type !== userType) || !logined.value) {
+            logout();
+            return navigateTo({ name: routesNames.login });
         }
     } else if (process.client) {
         await refresh();
-        if (!logined.value) {
-            return navigateTo({ name: routesNames.login });
-        }
-        if (userType && userData.value.type !== userType) {
+        if (!logined.value || (userType && userData.value.type !== userType)) {
             return navigateTo({ name: routesNames.login });
         }
     }
--- frontend/middleware/UserData.global.js
+++ frontend/middleware/UserData.global.js
@@ -1,31 +1,8 @@
-import axios from "axios";
 import { useAuthStore } from "@/stores/auth";
-import { storeToRefs } from "pinia";
 
 export default defineNuxtRouteMiddleware(async (context) => {
     if (process.server) {
         const authStore = useAuthStore();
-        const { userData, logined } = storeToRefs(authStore);
-        const cookie = useCookie("access_token_cookie");
-        if (cookie.value) {
-            try {
-                const request = await axios.get(
-                    "http://localhost:8000/api/v1/users/me",
-                    {
-                        withCredentials: true,
-                        headers: {
-                            Cookie: `access_token_cookie=${cookie.value}`,
-                        },
-                    }
-                );
-                if (request.status === 200) {
-                    userData.value = request.data;
-                    logined.value = true;
-                }
-            } catch (error) {}
-        }
-        if (!logined.value) {
-            authStore.logout();
-        }
+        await authStore.getUserData();
     }
 });
